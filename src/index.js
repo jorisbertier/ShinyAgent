0cc000b3c96696536aa3024eae03b8636c70ec50
Joris Bertier <joris.bertier@gmail.com>
2024-03-06 18:48:26 -0500

    initialisation root

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home/index';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Survey from './pages/Survey/Survey';
+import Header from './components/Header/Index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <Router>
+    <Header />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey" element={<Survey />} />
+      </Routes>
+            </Router>
+</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ function Home() {
 
   return (
     <div className="App">
+      <h1>Home</h1>
     </div>
   );
 }
