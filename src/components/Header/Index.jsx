524be4172a2467801435ea61db903e6db0e4cbc4
Joris Bertier <joris.bertier@gmail.com>
2024-03-07 15:41:45 -0500

    home & navbar

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/Color'
+import LogoLight from '../../assets/light-logo.png'
 
 const StyledLink = styled(Link)`
     padding: 15px;
@@ -15,15 +16,31 @@ const StyledLink = styled(Link)`
         color: blue; /* Changer la couleur au survol */
     }
 `
-
+const Cardimg = styled.img `
+    height: 96px;
+    width: 260px;
+`
+const Nav = styled.nav `
+display: flex;
+justify-content: space-between;
+align-items: center;
+background-color: grey;
+`
 function Header() {
     return (
-        <nav>
+        <Nav>
+            <Link to='/'>
+            <div>
+            <Cardimg src={LogoLight} alt='logo'></Cardimg>
+            </div>
+            </Link>
+        <div>
             <StyledLink to="/">Accueil</StyledLink>
-            <StyledLink to="/survey/1">Questionnaire</StyledLink>
             <StyledLink to="/freelances">Profils</StyledLink>
-            <StyledLink to="/result">Result</StyledLink>
-        </nav>
+            <StyledLink to="/survey/1" $isFullLink>Faire le test</StyledLink>
+            {/* <StyledLink to="/result">Result</StyledLink> */}
+        </div>
+        </Nav>
     )
     
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,40 @@
 import '../../styles/App.css';
 // import Freelances from './Freelances/Freelances';
+import HomeIllustration from '../../assets/home-illustration.svg'
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { StyledLink } from '../../utils/style/Atoms'
+
+const App = styled.div `
+  display: flex;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  margin: 30px;
+`
+const Presentation = styled.img `
+
+`
+
+const HomeContainer = styled.div `
+  width: 50%;
+  text-align: center;
+`
 
 function Home() {
 
   return (
-    <div className="App">
-      <h1>Home</h1>
+    <App>
+      <HomeContainer>
+        <h1>Repérer vos besoins,<br></br> on s'occupe du reste,<br></br>avec les meilleurs<br></br>talents</h1>
+        <StyledLink to='/survey/1' $isFullLink>Faire le test</StyledLink>
+      </HomeContainer>
+      <HomeContainer>
+
+      <Presentation src={HomeIllustration} alt="Home illustration"></Presentation>
+      </HomeContainer >
       {/* <Freelances /> */}
-    </div>
+    </App>
   );
 }
 
