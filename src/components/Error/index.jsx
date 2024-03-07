0486cefeb8aea5551728538391fd6955e24f67e8
Joris Bertier <joris.bertier@gmail.com>
2024-03-07 15:51:23 -0500

    style error

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,38 @@
+import styled from 'styled-components'
+import ErrorImg from '../../assets/404.svg'
+import colors from '../../utils/style/Color'
+
+const ErrorWrapper = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-height: 100vh;
+    max-width: 100vw;
+    flex-direction: column;
+`
+
+const Illustration = styled.img `
+width: 875px;
+height: 376px;
+top: 390px;
+left: 282px;
+`
+
+const ErrorTitle = styled.h1 `
+font-weight: 300;
+`
+const ErrorSubtitle = styled.h2 `
+font-weight: 300;
+color: ${colors.secondary}
+`
+
 function Error() {
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
-        </div>
+        <ErrorWrapper>
+            <ErrorTitle>Oups</ErrorTitle>
+            <Illustration src={ErrorImg} alt='error'></Illustration>
+            <ErrorSubtitle>Il semblerait que la page que vous cherchez n’existe pas</ErrorSubtitle>
+        </ErrorWrapper>
     )
 }
 
