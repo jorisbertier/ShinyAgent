1d34040dc00ba68f97621b7e4babad83d5686704
Joris Bertier <joris.bertier@gmail.com>
2024-03-22 13:10:37 -0500

    loader & api

--- src/pages/Survey/Survey.jsx
+++ src/pages/Survey/Survey.jsx
@@ -1,41 +1,73 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import colors from '../../utils/style/Color'
+import { Loader } from '../../utils/style/Atoms'
 
-const SurveyWrapper = styled.div `
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    width: 100vw;
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
-const NextPrevWrapper = styled.div `
-    display:flex;
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
 
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
 `
 
 function Survey() {
     
     const { questionNumber } = useParams()
-
     const questionNumberInt = parseInt(questionNumber)
-    const prevQuestion = questionNumberInt === 1 ? questionNumberInt === 1 : questionNumberInt -1;
-    const nextQuestion = questionNumberInt + 1;
-//questionNumberInt < 1 ? questionNumber === 1 : questionNumber -1
+    const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+    const nextQuestionNumber = questionNumberInt + 1
+    const [surveyData, setSurveyData] = useState({})
+    const [isDataLoading, setDataLoading] = useState(false)
+    
+    useEffect(() => {
+        setDataLoading(true)
+        fetch(`http://localhost:8000/survey`)
+            .then((response) => response.json()
+            .then(({ surveyData }) => {
+                setSurveyData(surveyData)
+                setDataLoading(false)
+            })
+            .catch((error) => console.log(error))
+            
+        )
+    }, [])
+
+    
     return (
-        <SurveyWrapper>
-            <h1>Questionnaire 🧮</h1>
-            <h2>Question {questionNumber}</h2>
-            <NextPrevWrapper>
-                <Link to={`/survey/${prevQuestion}`}>Précédent</Link>
-                {questionNumberInt === 10 ?
-                ( <Link to="/result">Result</Link> )
-                : (
-                    <Link to={`/survey/${nextQuestion}`} >Suivant</Link>
+        <SurveyContainer>
+            <QuestionTitle>Question {questionNumber}</QuestionTitle>
+            {isDataLoading ? <Loader /> : <QuestionContent>{surveyData[questionNumber]}   </QuestionContent>}
+            
+            <LinkWrapper>
+                <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+                    {surveyData[questionNumberInt + 1] ? (
+                <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+                ) : (
+                    <Link to="/results">Résultats</Link>
                 )}
-            </NextPrevWrapper>
-        </SurveyWrapper>
-    ) // questionNumberInt === 10 ? 
+            </LinkWrapper>
+        </SurveyContainer>
+    )
 }
 
 export default Survey
